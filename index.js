52ec472c3266727f8296fd2fd2d2920d24c74812
KN <knny.nguyen@gmail.com>
2018-12-10 17:49:47 -0500

    hide keys

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-// route handler for get request
-app.get('/', (req, res) => {
-  res.send({bye: 'buddy'});
-});
+passport.use(new GoogleStrategy());
 
 // app.listen: express telling node (the runtime) what port to listen to
 // process.env: look at the underlying environment and see if they declared a port
