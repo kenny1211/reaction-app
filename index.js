1c693bc32234e9f8206a7713afef085d919b3c01
KN <knny.nguyen@gmail.com>
2018-12-10 15:43:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// route handler for get request
+app.get('/', (req, res) => {
+  res.send({hi: 'there'});
+});
+
+// app.listen: express telling node (the runtime) what port to listen to
+// process.env: look at the underlying environment and see if they declared a port
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
+
+
