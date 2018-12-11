6aa1544d88ceb50432f89f8703282a7765c379d5
KN <knny.nguyen@gmail.com>
2018-12-11 17:27:01 -0500

    save user googleId to mongoDB

--- services/passport.js
+++ services/passport.js
@@ -1,21 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 // let passport know to use google strategy in the auth process
 passport.use(new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log('access token: ', accessToken);
-    console.log('refresh token: ', refreshToken);
-    console.log('profile: ', profile);
-    console.log('done: ', done);
-  }
-)
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      new User({ googleId: profile.id }).save();
+    }
+  )
 );
+
+
 
 
