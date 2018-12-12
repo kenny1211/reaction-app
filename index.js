a508c9a7a5c74b7d69e8343ef1934c7b6928bbbf
KN <knny.nguyen@gmail.com>
2018-12-12 14:19:34 -0500

    cookie session

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys')
 require('./models/User');
 require('./services/passport');
-
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //want cookie to last 30 days
+    keys: [keys.cookieKey] //encrypts cookie
+  })
+);
+// tell passport to use cookie
+app.use(passport.initialize());
+app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 // app.listen: express telling node (the runtime) what port to listen to
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User
+    .findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
 // let passport know to use google strategy in the auth process
 passport.use(new GoogleStrategy(
     {
