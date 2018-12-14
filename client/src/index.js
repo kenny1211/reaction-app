9d5877110c83609172d0410ab5b065bd27229b37
KN <knny.nguyen@gmail.com>
2018-12-14 10:53:18 -0500

    fetchUser action

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App';
 import reducers from './reducers';
 
 // 1st arg = reducers, 2nd arg initial state of app (server side rendering), 3rd = middleware
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   // Provider tag reads changes of store for redux/reducers that change state
