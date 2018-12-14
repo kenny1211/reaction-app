d89431d0d2082e44bebfd32f89a1f2fff9613d45
KN <knny.nguyen@gmail.com>
2018-12-14 11:44:10 -0500

    authReducer return

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,10 @@
+import { FETCH_USER } from '../actions/types';
+
 // deciding whether or not user is logged in
-export default function(state = {}, action) {
-  console.log(action);
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
