9565207c5d67242c87731faff5796f2f5a3ca4d4
KN <knny.nguyen@gmail.com>
2019-01-08 12:37:34 -0500

    survey instance - surveyRoutes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 
+//could require directly out of mongoose model class bc of possible issues when running tests
+const Survey = mongoose.model('surveys');
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      //recipients: take emails which is an array of strings and turn into array of objects
+      recipients: recipients.split(',').map(email => ({ email })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
