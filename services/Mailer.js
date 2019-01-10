27b2ce7458da8b1f9badf06abff87f0424500db1
KN <knny.nguyen@gmail.com>
2019-01-09 21:17:34 -0500

    email boilerplate

--- services/Mailer.js
+++ services/Mailer.js
@@ -12,6 +12,36 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    // built-in function from helper.Mail
+    this.addContent(this.body);
+    // enable click tracking inside of email defined below
+    this.addClickTracking();
+    // function to take and process list of recipients
+    this.addRecipients();
+  }
+
+  // function to destructure email property and correct helper email format
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+  // how send grid says to add click tracking
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+  // take list of helper email objects and add to Mailer
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
   }
 }
 
