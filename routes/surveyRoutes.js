d47755264f92ea00ae29a90acca97733ce17cd97
KN <knny.nguyen@gmail.com>
2019-01-11 16:14:09 -0500

    redirect domain

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 //could require directly out of mongoose model class bc of possible issues when running tests
 const Survey = mongoose.model('surveys');
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     // create survey instance
@@ -23,6 +23,17 @@ module.exports = app => {
 
     // send email
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+      await mailer.send();
+      // save survey to mongo
+      await survey.save();
+      // deduct credit and store into relevant user
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      // send back user model with update number of credits for header to catch
+      res.send(user);
+    } catch (err) {
+      res.status(422);
+    }
   });
 };
