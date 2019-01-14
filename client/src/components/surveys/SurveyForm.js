63eb330d3e903c767972261f7b1567f3aca353c9
KN <knny.nguyen@gmail.com>
2019-01-14 12:41:32 -0500

    SurveyForm component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,34 @@
 // SurveyForm shows a form fo a user to add input
 import React, { Component } from 'react';
 // reduxForm helps communicate to redux store at top of application, similar to connect function
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />;
+    });
+  }
+
   render() {
-    return <div>SurveyForm!</div>;
+    return (
+      <div>
+        {/* Field can show any type of input specified by props given to it */}
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
