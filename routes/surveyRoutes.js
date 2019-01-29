0e12e28f0d4ebd696a2a2602ce785192e786d8b8
KN <knny.nguyen@gmail.com>
2019-01-29 16:17:02 -0500

    surveys endpoint

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,13 @@ const { URL } = require('url');
 //could require directly out of mongoose model class bc of possible issues when running tests
 const Survey = mongoose.model('surveys');
 module.exports = app => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    // grab list of surveys for user that's logged in and exclude list of recipients
+    const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
+
+    res.send(surveys);
+  });
+
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
