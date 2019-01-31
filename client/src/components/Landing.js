65c32687e6844bd9c7ba5904d11a8f086c6f4b42
KN <knny.nguyen@gmail.com>
2019-01-30 19:37:18 -0500

    landing page style

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>
-        Reaction
-      </h1>
-        Collect feedback from your users
+    <div className="container">
+      <div className="row">
+        <h1>Getting Started</h1>
+      </div>
+      <div className="divider" />
+      <div className="row">
+        <div className="col">
+          <p>
+            Collect feedback from your users simply. Just signin with Google, create a survey and
+            the rest will be taken care of.
+          </p>
+        </div>
+        <div className="card-action">
+          <Link to="/surveys">Get Started</Link>
+        </div>
+      </div>
     </div>
   );
 };
